106e9dae9cea232402cdad11f40a7dcc3788cc02
Fridsco <27755972+Fridsco@users.noreply.github.com>
2020-07-22 12:55:06 -0500

    Update Tablas de multiplicar.cpp

--- Tablas de multiplicar.cpp
+++ Tablas de multiplicar.cpp
@@ -1,22 +1,24 @@
 #include <stdio.h>
 #include <windows.h>
 /*
-*Tablas de multiplicar en C, ingresas un número de tipo entero y te devuelve el resultado.
+*Fridsco
+*Tablas de multiplicar en C, ingresas un nÃºmero de tipo entero y te devuelve el resultado.
+*
 */
 main() //Inicio del programa
 {
-int X, Y, Z; //Declaración de variables.
+int X, Y, Z; //DeclaraciÃ³n de variables.
 int msgboxID=MessageBox(NULL, "Programado Por: Fridsco / Github       "," ", MB_ICONWARNING); //Mensaje emergente.
 Inicio: //Etiqueta: Al finzalizar el programa regresa al inicio.
 printf ("Ingrese un numero para obtener la tabla: "); //Imprime en pantalla.
 scanf("%d", &X); //Lee el teclado y lo guarda en la variable X.
-printf("\n"); //Imprime en pantalla un salto de línea.
+printf("\n"); //Imprime en pantalla un salto de lÃ­nea.
 for(Y=1; Y<=10; Y++) //Ciclo flor con la variable FR (FOR) y empieza el conteo desde 1 hasta llegar al 10.
 {
-	Z=X*Y; //Z adquiere el valor de la multiplicación de X por Y, es decir, X se multiplica 10 veces que es lo vale Y.
-	printf("--> %d X %d = %d\n", X, Y, Z); //Imprime en pantalla la multiplicación de Y con X
+	Z=X*Y; //Z adquiere el valor de la multiplicaciÃ³n de X por Y, es decir, X se multiplica 10 veces que es lo vale Y.
+	printf("--> %d X %d = %d\n", X, Y, Z); //Imprime en pantalla la multiplicaciÃ³n de Y con X
 }
-printf("\n"); //Imprime en pantalla un salto de línea.
+printf("\n"); //Imprime en pantalla un salto de lÃ­nea.
 system("pause"); //Hace una pausa hasta que se precione cualquier letra.
 system("cls"); //Limpia la pantalla.
 goto Inicio; //Regtresa al inicio del programa (Etiqueta).
