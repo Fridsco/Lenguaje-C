5170d33325c5332a0e590641d9e63c404ddf9a23
Fridsco <27755972+Fridsco@users.noreply.github.com>
2020-10-03 23:48:42 -0500

    Rename Abecedario.cpp to Abecedario con for.cpp

--- Abecedario con for.cpp
+++ Abecedario con for.cpp
@@ -1,17 +1,17 @@
-#include <conio.h>	//función getche()
+#include <conio.h>	//funciÃ³n getche()
 #include <stdio.h>	//puts, scanf, etc
 
 /*
 * Abecedario con For
 * Escrito por Fridsco
-* En el código ASCII, el abecedario empieza desde el número 64 (En mayúsculas) y termina en 90.
-* Entonces lo que se hace aquí, es imprimir la tabla.
+* En el cÃ³digo ASCII, el abecedario empieza desde el nÃºmero 64 (En mayÃºsculas) y termina en 90.
+* Entonces lo que se hace aquÃ­, es imprimir la tabla.
 */
 
 main()
 {						  																  
-	int X, Y; // Declaración de variables.
-	char ABC = 65; // La letra "A" empieza en el número 65 de la tabla y la Z es 90. ASCII.
+	int X, Y; // DeclaraciÃ³n de variables.
+	char ABC = 65; // La letra "A" empieza en el nÃºmero 65 de la tabla y la Z es 90. ASCII.
 	for(X=65; X<=90; X++) //El ciclo for empieza en 65.
 	{																		
 		printf("\n---> %c", X, ABC); // Imprime en pantalla el abecedario usando ASCII						   	   
