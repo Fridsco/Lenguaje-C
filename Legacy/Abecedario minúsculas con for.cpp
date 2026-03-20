16748999e2c2e254bbfb4087318d22ac8b9c29f5
Fridsco <27755972+Fridsco@users.noreply.github.com>
2026-03-20 03:37:04 +0000

    Rename Abecedario minúsculas con for.cpp to Legacy/Abecedario minúsculas con for.cpp

--- Legacy/Abecedario minúsculas con for.cpp
+++ Legacy/Abecedario minúsculas con for.cpp
@@ -1,17 +1,17 @@
-#include <conio.h>	//función getche()
+#include <conio.h>	//funciÃ³n getche()
 #include <stdio.h>	//puts, scanf, etc
 
 /*
 * Abecedario con For
 * Escrito por Fridsco
-* En el código ASCII, el abecedario empieza desde el número 97 (En minúsculas) y termina en 122.
-* Entonces lo que se hace aquí, es imprimir la tabla.
+* En el cÃ³digo ASCII, el abecedario empieza desde el nÃºmero 97 (En minÃºsculas) y termina en 122.
+* Entonces lo que se hace aquÃ­, es imprimir la tabla.
 */
 
 main()
 {						  																  
-	int X, Y; // Declaración de variables.
-	char abc = 97; // La letra "97" empieza en el número 97 de la tabla y la "z" es 122. ASCII.
+	int X, Y; // DeclaraciÃ³n de variables.
+	char abc = 97; // La letra "97" empieza en el nÃºmero 97 de la tabla y la "z" es 122. ASCII.
 	for(X=97; X<=122; X++) //El ciclo for empieza en 97.
 	{						
 		printf("\n---> %c", X, abc); // Imprime en pantalla el abecedario usando ASCII						   	   
